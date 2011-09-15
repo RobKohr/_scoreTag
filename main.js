d7ad703ada1e34488462517d4027cf3ffcb10fea
Rob Kohr <spam@robkohr.com>
2011-09-15 06:40:15 -0700

    readme updated

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
     var url = "http://www.google.com/search?q=site%3Aplus.google.com+%22$1%22&amp;tbs=qdr:y,sbd:1&"
     setInterval(function(){
-	$('.vg, .zj').not('.scored').replaceText(/(^\_[a-zA-Z0-9\_\-]+|\ \_[a-zA-Z0-9\_\-]+)/gi, '<a href="'+url+'">$1</a>').addClass('scored');
+	//.vg is post body, and .zj is comment body. 
+	$('.Sx div, .vg, .zj').
+	    not('.scored').
+	    replaceText(/(^\_[a-zA-Z0-9\_\-]+)/gi, '<a href="'+url+'">$1</a>').  //start of element
+	    replaceText(/\ (\_[a-zA-Z0-9\_\-]+)/gi, ' <a href="'+url+'">$1</a>'). //after a space
+	    addClass('scored');
     }, 2000);
 });
